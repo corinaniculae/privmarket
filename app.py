468083afe90979e5eff29c8599c14532440612c3
corinaniculae <cn1612@imperial.ac.uk>
2016-05-27 00:48:17 +0100

    Added test for map plotting.

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 # Main routing file for mapping the webapp requests.
 
-from flask import Flask, render_template
-app = Flask(__name__)
+import flask
+import gmplot
+import shutil
+
+
+app = flask.Flask(__name__)
 
 
 # Routing the webapp's main URL GET request.
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
+
+
+def _generate_html_map():
+    gmap = gmplot.GoogleMapPlotter(51.4700, 0.4543, 10)
+    gmap.plot([51.4988], [0.1749], 'cornflowerblue', edge_width=10)
+    gmap.draw("mymap.html")
+    shutil.move("./mymap.html",
+                "./templates/mymap.html")
+
+
+@app.route('/test')
+def get_tube_map():
+    _generate_html_map()
+    return flask.render_template('mymap.html')
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
